ba499c45fad25b93c3b42c4859837c76fc644567
Karthik-6362 <karthikappu94485@gmail.com>
2025-04-17 17:38:45 +0000

    Changes were made locally and updated

--- tests/dut_test.py
+++ tests/dut_test.py
@@ -4,4 +4,15 @@ from cocotb.triggers import Timer
 
 @cocotb.test()
 async def dut_test(dut):
-    assert 0, "Test Not Implemented Error"
+    a = [0,0,1,1]
+    b = [0,1,0,1]
+    c = [0,1,1,0]
+    
+    for i in range(4):
+    	dut.a.value = a[i]
+    	dut.b.value = b[i]
+    	await Timer(1,'ns')
+    	if dut.y.value != c[i]:
+    		print(f"Error in interation {i}")
+    	else:
+    		print(f"Testcase in iter")
